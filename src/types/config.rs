cfd01446523054a1098e06abfb42f72f5a627eda
TheAwiteb <awiteb@hotmail.com>
2022-03-22 22:58:54 +0300

    🧹: Remove unused import

--- src/types/config.rs
+++ src/types/config.rs
@@ -13,7 +13,6 @@
 // You should have received a copy of the GNU Affero General Public License along
 // with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-use crate::types;
 use crate::types::Domain;
 
 /// [`Config`] struct help you to manage domains with [`Domain`] struct
